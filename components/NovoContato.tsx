2d7607a038c36c1608ff08f9420c8cf263d26abf
Elian Batista <elianbatista2000@gmail.com>
2021-04-22 11:40:18 -0300

    feat: create the NovoContato component

---
+++ components/NovoContato.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from '../styles/components/NovoContato.module.css'
+
+const NovoContato = () => {
+  return (
+    <div className={styles.container}>
+      <h1>
+        Criar novo contato
+      </h1>
+      <form className={styles.form}>
+        <div className={styles.field}>
+          <label>Nome</label>
+          <input type="text"/>
+        </div>
+        <div className={styles.field}>
+          <label>E-mail</label>
+          <input type="text"/>
+        </div>
+        <div className={styles.field}>
+          <label>Telefone</label>
+          <input type="text"/>
+        </div>
+      </form>
+      <div className={styles.content_buttons}>
+        <button>
+          Cancelar
+        </button>
+        <button>
+          Salvar
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default NovoContato
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import styles from '../styles/pages/Home.module.css'
 
 import Topo from '../components/Topo'
 import SemContato from '../components/SemContato'
+import PopUpManagament from '../components/PopupManagement'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <PopUpManagament />
       <Topo />
       <SemContato />
     </div>
